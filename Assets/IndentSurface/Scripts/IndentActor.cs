982fea642470bf4efcbba7abd1e80f58512df47f
Max-JK <maks32110@mail.ru>
2018-04-07 19:44:10 +1000

    Revert "Update IndentActor.cs"

--- Assets/IndentSurface/Scripts/IndentActor.cs
+++ Assets/IndentSurface/Scripts/IndentActor.cs
@@ -1,35 +1,42 @@
-using UnityEngine;
-namespace Wacki.IndentSurface {
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Wacki.IndentSurface
+{
     /// <summary>
     /// Simple control script for our sphere that leaves a track in the snow.
     /// </summary>
-    public class IndentActor : MonoBehaviour {
+    public class IndentActor : MonoBehaviour
+    {
         [Range(0.0f, 0.2f)]
-        [SerializeField] private float drawDelta = 0.01f;
+        public float drawDelta = 0.01f;
         private Vector3 _prevDrawPos;
-
-        private void FixedUpdate() {
+        
+        void Update()
+        {
             float v = Input.GetAxis("Vertical");
             float h = Input.GetAxis("Horizontal");
 
             GetComponent<Rigidbody>().AddTorque(v, 0, -h);
-        }
-        
-        private void OnCollisionStay(Collision collider) {
-            if (Vector3.Distance(_prevDrawPos, transform.position) < drawDelta) return;
+
+            if (Vector3.Distance(_prevDrawPos, transform.position) < drawDelta)
+                return;
+
+            _prevDrawPos = transform.position;
+
+            Debug.DrawLine(transform.position, transform.position + Vector3.down);
 
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, Vector3.down, out hit) && collider.collider == hit.collider) {
-                var texDraw = collider.gameObject.GetComponent<IndentDraw>();
-
-                if (texDraw == null) return;
-                Debug.DrawLine(transform.position, transform.position + Vector3.down);
+            if (Physics.Raycast(transform.position, Vector3.down, out hit))
+            {
+                var texDraw = hit.collider.gameObject.GetComponent<IndentDraw>();
+                if (texDraw == null)
+                    return;
 
                 texDraw.IndentAt(hit);
             }
-            _prevDrawPos = transform.position;
         }
-
     }
 
 }
